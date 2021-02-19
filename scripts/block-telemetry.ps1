8936e51ebf2d6a8fc7b06b6c26501fed06212b27
AJESHKUMAR T G <46497585+ajeshkumartg@users.noreply.github.com>
2021-02-19 10:52:28 +0530

    Update block-telemetry.ps1

--- scripts/block-telemetry.ps1
+++ scripts/block-telemetry.ps1
@@ -8,7 +8,7 @@
 # Please see the related issue:
 # <https://github.com/W4RH4WK/Debloat-Windows-10/issues/79>
 
-# Self-elevate the script if required
+
 if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
  if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
   $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
