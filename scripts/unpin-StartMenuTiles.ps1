5beb50e8a9be3f457e64f3fea552d86848f61f32
AJESHKUMAR T G <46497585+ajeshkumartg@users.noreply.github.com>
2021-02-19 11:05:36 +0530

    Added unpin-StartMenuTiles

---
+++ scripts/unpin-StartMenuTiles.ps1
@@ -0,0 +1,8 @@
+#Unpins all tiles from the Start Menu
+    Write-Output "Unpinning all tiles from the start menu"
+    (New-Object -Com Shell.Application).
+    NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').
+    Items() |
+    %{ $_.Verbs() } |
+    ?{$_.Name -match 'Un.*pin from Start'} |
+    %{$_.DoIt()}
