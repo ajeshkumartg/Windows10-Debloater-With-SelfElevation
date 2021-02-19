91b3acb4ce5538d343e7d077d8d5089a341bdd1d
AJESHKUMAR T G <46497585+ajeshkumartg@users.noreply.github.com>
2021-02-19 11:01:07 +0530

    Update fix-privacy-settings.ps1

--- scripts/fix-privacy-settings.ps1
+++ scripts/fix-privacy-settings.ps1
@@ -2,7 +2,6 @@
 # This script will try to fix many of the privacy settings for the user. This
 # is work in progress!
 
-# Self-elevate the script if required
 if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
  if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
   $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
