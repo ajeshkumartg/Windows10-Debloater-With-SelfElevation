ad58103c3bb64e74c601657b5a154a69f7742385
AJESHKUMAR T G <46497585+ajeshkumartg@users.noreply.github.com>
2021-02-19 11:02:24 +0530

    Update optimize-windows-update.ps1

--- scripts/optimize-windows-update.ps1
+++ scripts/optimize-windows-update.ps1
@@ -2,7 +2,6 @@
 # This script optimizes Windows updates by disabling automatic download and
 # seeding updates to other computers.
 
-# Self-elevate the script if required
 if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
  if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
   $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
