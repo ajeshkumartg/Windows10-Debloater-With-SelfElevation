872867d5d8af2341adbba157955f006f9a482838
AJESHKUMAR T G <46497585+ajeshkumartg@users.noreply.github.com>
2021-02-19 11:06:28 +0530

    Added unpin-StartMenuTiles

--- scripts/unpin-StartMenuTiles.ps1
+++ scripts/unpin-StartMenuTiles.ps1
@@ -1,4 +1,12 @@
 #Unpins all tiles from the Start Menu
+if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+ if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
+  $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
+  Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
+  Exit
+ }
+}
+
     Write-Output "Unpinning all tiles from the start menu"
     (New-Object -Com Shell.Application).
     NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').
