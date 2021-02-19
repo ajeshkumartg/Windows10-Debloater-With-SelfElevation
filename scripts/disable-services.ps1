8366e3001f41cfd4c353ede1c04affe9c3bf2c45
AJESHKUMAR T G <46497585+ajeshkumartg@users.noreply.github.com>
2021-02-19 10:56:44 +0530

    Update disable-services.ps1

--- scripts/disable-services.ps1
+++ scripts/disable-services.ps1
@@ -2,7 +2,7 @@
 # This script disables unwanted Windows services. If you do not want to disable
 # certain services comment out the corresponding lines below.
 
-# Self-elevate the script if required
+
 if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
  if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
   $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
@@ -22,7 +22,7 @@ $services = @(
     "RemoteRegistry"                           # Remote Registry
     "SharedAccess"                             # Internet Connection Sharing (ICS)
     "TrkWks"                                   # Distributed Link Tracking Client
-    "WbioSrvc"                                 # Windows Biometric Service (required for Fingerprint reader / facial detection)
+    #"WbioSrvc"                                # Windows Biometric Service (required for Fingerprint reader / facial detection)
     #"WlanSvc"                                 # WLAN AutoConfig
     "WMPNetworkSvc"                            # Windows Media Player Network Sharing Service
     #"wscsvc"                                  # Windows Security Center Service
